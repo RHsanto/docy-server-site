ba7e7591d88db3f5fc5fba16979d041a2c9647cd
Rhsanto <rakibulsanto789@gmail.com>
2022-03-05 01:08:03 +0600

    check gitHub

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// mongo url and client
+// mongo url and client //
+// mongo url and client //
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.avm9c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
